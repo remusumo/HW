a265875ea5fbab0845204105a7be333e94a816f8
sid <qazwsx8557@gmail.com>
2020-03-20 03:14:30 -0700

    Initial Commit

---
+++ HW/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW
+//
+//  Created by qazwsx8557 on 2020/3/20.
+//  Copyright © 2020 qazwsx8557. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
